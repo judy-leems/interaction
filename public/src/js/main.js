299e28a414bda37a5967f5e5755aac753382a212
minsun <yamyam@pentabreed.com>
2020-11-16 22:43:19 +0900

    scroll 높이 설정 중

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,1 +1,56 @@
-'use strict'
+'use strict';
+// 전역변수 선언을 피하려고 IIFE 방식으로 선언 전역변수 선언을 해주는건 별로 좋은방식이 아님. 
+(() => {
+  const sceneInfo = [
+    {
+      // 0
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅 > 스크롤할 높이가 너무 짧으면 애니메이션 구현이 짧게됨 
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0')
+      }
+    },
+    {
+      // 1
+      type: 'normal',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅 
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1')
+      }
+    },
+    {
+      // 2
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅 
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2')
+      }
+    },
+    {
+      // 3
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅 
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3')
+      }
+    }
+  ]
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅 
+    for(let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+    console.log(sceneInfo);
+  }
+
+  setLayout();
+
+  window.addEventListener('resize', setLayout);
+
+})();
