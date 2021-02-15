c6f8b5b6bd27dfade25a0b7a2586fb81b720d681
minsun <yamyam@pentabreed.com>
2021-02-15 23:55:44 +0900

    활성 씬 작업 완료

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,6 +1,11 @@
 'use strict';
-// 전역변수 선언을 피하려고 IIFE 방식으로 선언 전역변수 선언을 해주는건 별로 좋은방식이 아님. 
+// 전역변수 선언을 피하려고 IIFE 방식으로 선언 전역변수, 선언을 해주는건 별로 좋은방식이 아님. 
 (() => {
+
+  let yOffset = 0; // winodw.pageYOffset 대신 쓸 변수 
+  let prevScrollHeight = 0; // 현재 스크롤 위치 (yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이들의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
   const sceneInfo = [
     {
       // 0
@@ -8,7 +13,15 @@
       heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅 > 스크롤할 높이가 너무 짧으면 애니메이션 구현이 짧게됨 
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-0')
+        container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d')
+      },
+      values: {
+        messageA_opacity: [0, 1],
+
       }
     },
     {
@@ -46,11 +59,54 @@
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo);
+
+    // 새로고침 
+    yOffset = window.pageYOffset
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
-  setLayout();
+  function scrollLoop() {
 
-  window.addEventListener('resize', setLayout);
+    prevScrollHeight = 0; // 값이 누적되어 계속 커지기 때문에 초기화를 한번 해줘야함 
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) { // 1번씬에서 2번씬으로 넘어갈 때는 1번씬이 모두 다 지나고 2번씬이 변해야 하기 때문에 
+      currentScene++;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    }
+
+    if(yOffset < prevScrollHeight) { // 2번씬에서 1번씬으로 갈 경우에는 1번씩에 진입하자마자 변한다.
+      if (currentScene === 0) return; //  ios 브라우저에서 종종 -1 나올 경우가 있어서 0 일때 return 
+      currentScene--;
+      document.body.setAttribute('id', `show-scene-${currentScene}`);
+    } 
+
+   
+    
+  }
+
+
+  window.addEventListener('load', () => {
+    setLayout();
+  })
+  window.addEventListener('resize', () => {
+    setLayout();
+  })
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  })
+  
 
 })();
